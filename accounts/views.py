93650df1b3adbaf22d8df2d55391b6d748e7773a
0406Masa <spr2540092@stu.o-hara.ac.jp>
2025-11-17 14:51:03 +0900

    new start

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,19 @@
-from django.views.generic import CreateView, TemplateView
-from .forms import CustomUserCreationForm
-from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView   # TemplateView を追加
 from django.contrib.auth import login
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
+# サインアップビュー
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy('accounts:signup_success')
+    success_url = reverse_lazy('accounts:signup_success')  # サインアップ成功ページ
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        login(self.request, self.object)  # サインアップ後自動ログイン
+        login(self.request, self.object)  # 自動ログイン
         return response
 
+# サインアップ成功ページビュー
 class SignUpSuccessView(TemplateView):
     template_name = "accounts/signup_success.html"
--- testproject/urls.py
+++ testproject/urls.py
@@ -1,15 +1,28 @@
+"""
+URL configuration for testproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # testapp のページ（トップ）
-    path('', include('testapp.urls')),
-
-    # accounts アプリ（signup, signup_success）
-    path('accounts/', include('accounts.urls')),
-
-    # Django 標準の login/logout
+    path('', include('testapp.urls')),        # トップページ
+    path('', include('accounts.urls')),       # サインアップ用
     path('accounts/', include('django.contrib.auth.urls')),
 ]
